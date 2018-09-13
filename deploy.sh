494b986473897d1f1e91906062d947e3d87c5ddc
Heebum Kwak <heebum.kwak@linecorp.com>
2018-09-13 16:46:40 +0900

    Apply deploy approach

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# abort on errors
+set -e
+
 # build
 npm run build
 
+# navigate into the build output directory
 cd docs/.vuepress/dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/teamlck/teamlck.git master:gh-pages
+
+cd -
 
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  base: '/teamlck/',
   locales: {
     '/': {
       lang: 'ko-KR',
@@ -15,6 +16,7 @@ module.exports = {
   markdown: {
     lineNumbers: true
   },
+  theme: 'vue',
   themeConfig: {
     repo: 'teamlck/teamlck',
     editLinks: true,
